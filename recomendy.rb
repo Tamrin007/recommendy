96de274167cbd09d59a9e79034f0c2540cab4e66
Tamrin007 <tamrin.bitterchococo@gmail.com>
2016-09-03 16:54:14 +0900

    Webhookの設定

--- recomendy.rb
+++ recomendy.rb
@@ -30,3 +30,10 @@ get '/' do
   # end
   # rows
 end
+
+get "/callback" do
+    if params["hub.verify_token"] != "EAADESXvPAzwBADFMPxnegfbUJ86jzgPW7vxw6nGuX9ZAy4Wwz0ubG67qRiWpfjMfNwQuofJQkbc1Msx5aNLMr5AZBmm5IFUKBLP7F30zHewXgmpyRqqeYCZBmWQT4k1jzRZAdM7G4M7J7dUrcPgfkiMJmas6BprQ2RLZCJiUljQZDZD"
+        return "Error, wrong validation token"
+    end
+    params["hub.challenge"]
+end
